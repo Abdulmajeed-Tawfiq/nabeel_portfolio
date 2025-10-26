21bf7bb99799684955f482a6388d2d2a1f12f573
Abdulmajeed-Tawfiq <abdulmajeedtawfiq98@gmail.com>
2025-10-26 22:17:07 +0300

    fix build bug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Github, Linkedin, Mail, Phone } from "lucide-react";
+import { Linkedin, Mail, Phone } from "lucide-react";
 
 function Footer() {
   return (
